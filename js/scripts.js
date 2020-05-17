12561cb8b936da69bde9db0b032295c4fe7c3401
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-17 19:49:18 +0300

    Service icons can be toggled

--- js/scripts.js
+++ js/scripts.js
@@ -2,20 +2,32 @@ $(document).ready(function(){
     $("#discription1").hide();
     $("#discription2").hide();
     $("#discription3").hide();
-    
+
     $("#design").click(function(){
         $("#discription1").toggle();
         $(".design").toggle();
+    });   
+    $("#discription1").click(function(){
+        $(".design").toggle();
+        $("#discription1").toggle();
     });   
     
     $("#development").click(function(){
         $("#discription2").toggle();
         $(".develop").toggle();
     });   
+    $("#discription2").click(function(){
+        $(".develop").toggle();
+        $("#discription2").toggle();
+    });   
     
     $("#product").click(function(){
         $("#discription3").toggle();
         $(".produce").toggle();
+    });   
+    $("#discription3").click(function(){
+        $(".produce").toggle();
+        $("#discription3").toggle();
     });   
     
 });
