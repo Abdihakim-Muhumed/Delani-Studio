34f58fda9314dd1aff68ac2220841208a1149549
Abdihakim-Muhumed <abdihakim.0017@gmail.com>
2020-05-17 17:23:03 +0300

    Edit index.htm, styles.css and scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,21 @@
 $(document).ready(function(){
     $("#discription1").hide();
-    $(".design").click(function(){
+    $("#discription2").hide();
+    $("#discription3").hide();
+    
+    $("#design").click(function(){
         $("#discription1").toggle();
         $(".design").toggle();
     });   
-    $("#design").hover(function(){
-        $("#discription1").toggle();
-        $(".design").toggle();
+    
+    $("#development").click(function(){
+        $("#discription2").toggle();
+        $(".develop").toggle();
     });   
+    
+    $("#product").click(function(){
+        $("#discription3").toggle();
+        $(".produce").toggle();
+    });   
+    
 });
